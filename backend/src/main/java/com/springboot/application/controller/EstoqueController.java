144d9aa6cfe73d65e3f935e176495e6c96130805
SantosPereira <pedrohenriquelemam@gmail.com>
2022-01-22 23:43:07 -0300

    Resilvido erro CORS

--- backend/src/main/java/com/springboot/application/controller/EstoqueController.java
+++ backend/src/main/java/com/springboot/application/controller/EstoqueController.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.application.model.Estoque;
 import com.springboot.application.repository.EstoqueRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/estoque")
 // @Controller
--- frontend/src/views/Usuario.vue
+++ frontend/src/views/Usuario.vue
@@ -1,59 +1,62 @@
 <template>
-    <div>
-        <h1>Usuários</h1>
-        <section>
-            <h3>Lista de Usuários</h3>
-            <form action="/listar" method="get">
-                <input type="text" placeholder="Nome">
-                <input type="submit" value="Pesquisar">
-            </form>
-            <button>Cadastrar Usuário</button>
-            <table>
-                <tr>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Usuário</th>
-                    <th>Filial</th>
-                    <th>Setor</th>
-                    <th>Função</th>
-                    <th>Ações</th>
-                </tr>
-                <!-- <tr v-for="usuario in usuarios">
-                    <td>{{ usuario.nome }}</td>
-                    <td>{{ usuario.email }}</td>
-                    <td>{{ usuario.nomeUsuario }}</td>
-                    <td>{{ usuario.filial }}</td>
-                    <td>{{ usuario.setor }}</td>
-                    <td>{{ usuario.funcao }}</td>
-                    <td>
-                        <button>Editar</button>
-                        <button>Remover</button>
-                    </td>
-                </tr> -->
-                {{usuarios}}
-            </table>
-        </section>
-    </div>
+  <div>
+    <h1>Usuários</h1>
+    <section>
+      <h3>Lista de Usuários</h3>
+      <form action="/listar" method="get">
+        <input type="text" placeholder="Nome" />
+        <input type="submit" value="Pesquisar" />
+      </form>
+      <button>Cadastrar Usuário</button>
+      <table>
+        <tr>
+          <th>Nome</th>
+          <th>Email</th>
+          <th>Usuário</th>
+          <th>Filial</th>
+          <th>Setor</th>
+          <th>Função</th>
+          <th>Ações</th>
+        </tr>
+        <tr v-for="usuario in usuarios" :key="usuario.id">
+            <td>{{ usuario.nome }}</td>
+            <td>{{ usuario.email }}</td>
+            <td>{{ usuario.nomeUsuario }}</td>
+            <td>{{ usuario.filial }}</td>
+            <td>{{ usuario.setor }}</td>
+            <td>{{ usuario.funcao }}</td>
+            <td>
+                <button>Editar</button>
+                <button>Remover</button>
+            </td>
+        </tr>
+      </table>
+        <!-- {{ usuarios }} -->
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
-    name: "Usuario",
-    data: function() {
-        return {
-            cabecalho: {
-                headers: new Headers().append("Access-Control-Allow-Origin","*")
-                },
-            usuarios: fetch('http://localhost:8080/usuario/listar', this.cabecalho).then(resposta => {
-                console.log(resposta.json())
-                return resposta.json()
-                }),
+import axios from "axios";
+var cabec = new Headers();
+cabec.append("Access-Control-Allow-Origin","*")
+// var req = {headers: {"Access-Control-Allow-Origin":"*"}};
 
-        }
+export default {
+  name: "Usuario",
+  data: function () {
+    return {
+        usuarios: [],
     }
+  },
+  created() {
+    axios.get("http://localhost:8080/usuario/listar", cabec)
+        .then((resposta) => {
+            this.usuarios = resposta.data;
+            }
+        )
+    },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
